a77c0b6c30be8019cae279ee185db1d662835231
Anton <anton@adc.sh>
2020-01-08 07:08:34 +0300

    compile

---
+++ src/depack.jsx
@@ -0,0 +1,5 @@
+import { render } from 'preact'
+import '../types/externs'
+import PhotoUploader from './'
+
+render(<PhotoUploader />, window['container'])
--- types/externs.js
+++ types/externs.js
@@ -10,3 +10,10 @@ var _photoUploader = {}
  * @typedef {{ recognition: (string|undefined), drop: (string|undefined), previewLoading: (string|undefined), upload: (string|undefined), uploadAgain: (string|undefined), link: (string|undefined), error: (string|undefined), serverProcessing: (string|undefined) }}
  */
 _photoUploader.Locale
+
+/* typal types/index.xml externs */
+/**
+ * The photo uploader will create preview elements for each photo and have a hidden input field populated with the `photoId` received when the photo was updated.
+ * @typedef {{ fieldName: (string|undefined), uploadedResults: (string|undefined), uploadUri: string, LOCALE: (_photoUploader.Locale|undefined), locale: (string|undefined), onRemove: ((function(!Object): void)|undefined), onAdded: ((function(): void)|undefined), onPhotoUploaded: ((function(!Object): void)|undefined) }}
+ */
+_photoUploader.Props
